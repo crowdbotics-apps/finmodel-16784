e5ba4fc01e195f5cf241529ef1b042dda49b527f
Crowdbotics <team@crowdbotics.com>
2020-05-09 03:12:45 +0000

    Update models

--- finmodel_16784/wsgi.py
+++ finmodel_16784/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finmodel_16784.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finmodel_16784.settings")
 
 application = get_wsgi_application()
